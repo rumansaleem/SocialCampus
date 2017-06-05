7ba59ce0967b43ed217464017fbef47913c0bdc7
Ruman Saleem <ruman63@gmail.com>
2017-06-05 15:28:11 +0530

    Custom Home Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Social Campus
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact Us</a>
+                    <a href="#">Help</a>
                 </div>
             </div>
         </div>
