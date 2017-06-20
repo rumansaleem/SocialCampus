47f61710a7aea3feec6fecba799de1b736e470c1
Ruman Saleem <ruman63@gmail.com>
2017-06-20 15:14:24 +0530

    Make Layouts and Partials

---
+++ resources/views/partials/_head.blade.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<title>@yield('title') CampusNetwork.com</title>
+<meta name="author" content="Campus Network" />
+<meta name="description" content="The Campus Social Network" />
+<!-- Don't forget to set your site up: http://google.com/webmasters -->
+<meta name="google-site-verification" content="" />
+<meta name="Copyright" content="" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="{{mix('css/app.css')}}">
+@yield('stylesheets')
