4552f2316116157c86e2a47e9b89b6bde400be06
Ruman Saleem <ruman63@gmail.com>
2017-06-29 18:43:13 +0530

    Fixed Body Padding Removed Merge backups Fixed Errors created by auto merge

--- public/js/app.js
+++ public/js/app.js
@@ -801,7 +801,7 @@ __webpack_require__(43);
 /* 9 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n\n^\n      Invalid CSS after \"\": expected 1 selector or at-rule, was \"<<<<<<< HEAD\"\n      in /home/vagrant/Sites/SocialCampus/resources/assets/sass/_variables.scss (line 1, column 1)\n    at runLoaders (/home/vagrant/Sites/SocialCampus/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /home/vagrant/Sites/SocialCampus/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/vagrant/Sites/SocialCampus/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/vagrant/Sites/SocialCampus/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/vagrant/Sites/SocialCampus/node_modules/sass-loader/lib/loader.js:57:13)\n    at Object.<anonymous> (/home/vagrant/Sites/SocialCampus/node_modules/sass-loader/node_modules/async/dist/async.js:2243:31)\n    at Object.callback (/home/vagrant/Sites/SocialCampus/node_modules/sass-loader/node_modules/async/dist/async.js:906:16)\n    at options.error (/home/vagrant/Sites/SocialCampus/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 /* 10 */
